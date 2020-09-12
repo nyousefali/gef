d5a37aa68c7a9155c3a0e17569aafaf8aea0bc1c
Násser Yousef Ali <nasser.yousef.unb@gmail.com>
2020-09-12 17:47:32 -0300

    Finalizing the project GEF Biogas Brasil.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-/*$(document).ready(function () {
-    $(".icon").click(function () {
-      $(this).toggleClass("icon-active");
-    });
-  });*/
-  
+    const controles = document.getElementById('controles');
+    const entrada = document.getElementById('entrada');
+    controles.addEventListener('change', handleChange);
+
+    function handleChange(event){
+        const value = event.target.value;
+
+        if (isNaN(document.getElementById('entrada').value)) {
+            document.getElementById('resultado').value = "Não é um número!"
+        }else{
+            document.getElementById('resultado').value = document.getElementById('entrada').value * value;
+        }
+
+        //document.getElementById('resultado').value = document.getElementById('entrada').value * value;
+    }
